b3e831bd7db91f0e9ed43f918f00e8ea40ff2fb8
alperencngz <cengiz.ozturk@ozu.edu.tr>
2023-06-21 09:22:59 +0300

    Solved anchor tag reload problem
    When providing a route with anchor tag as a link, whole app realoads when clicked. Instead use Link element of react-router-dom

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,14 @@
-function HomePage () {
-    return <h1>Homepage Welcome</h1>
+import { Link } from "react-router-dom";
+
+function HomePage() {
+    return (
+        <>
+            <h1>Homepage Welcome</h1>
+            <p>
+                Go to <Link to="/products">Products</Link>.
+            </p>
+        </>
+    )
 }
 
 export default HomePage;
