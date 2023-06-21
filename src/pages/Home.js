648a9555faa7eac2cb23abcf9216b36b2cf067e3
alperencngz <cengiz.ozturk@ozu.edu.tr>
2023-06-21 15:54:51 +0300

    Navigating programmatically
    Don't use this on a button, usually onclick will be connected with a link etc but this is for the demo

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,21 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 function HomePage() {
+
+    const navigate = useNavigate();
+
+    function navigateHandler() {
+        navigate("/products");
+    }
+
     return (
         <>
             <h1>Homepage Welcome</h1>
             <p>
                 Go to <Link to="/products">Products</Link>.
+            </p>
+            <p>
+                <button onClick={navigateHandler}>Navigate</button>
             </p>
         </>
     )
